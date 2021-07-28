c779f8a4b9794ed8a9a50d8a56afedc4a0bb8452
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-07-28 22:33:47 +0200

    refactored

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  entry: path.resolve(__dirname, "./src/index.tsx"),
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
@@ -20,13 +20,13 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, "..", "./build"),
+    path: path.resolve(__dirname, "./build"),
     filename: "bundle.js",
   },
   mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
+      template: path.resolve(__dirname, "./public/index.html"),
     }),
   ],
 };
